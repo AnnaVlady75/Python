6079e7fce389d5ab4e1bc330d8d03d0370b6d578
AnnaVlady <7753277av@gmail.com>
2023-05-27 22:11:22 +0300

    added folder Telephone_book

--- Telephone_book/view.py
+++ Telephone_book/view.py
@@ -1,17 +1,23 @@
 def user_input():
-    ask = int(input("Выбери ниже\n 1- записать пользователя\n"))
+    ask = int(input("Выбери ниже\n 1- записать пользователя\n 2- найти по имени\n"
+                    "3-сортировка по имени\n 4-сортировка по дате рождения\n"
+                    "5- вывод только имени\n 6- вывод всего файла\n"
+                    "7-удалить\n 8- изменить\n 9- выход\n"))
     return ask
 
-def man():
+def person():
     name = input("введите имя: ")
-    family = input("введите фамилию: ")
-    father = input("введите отчество: ")
-    date = input("введите дату рождения: ")
+    last_name = input("введите фамилию: ")
+    patronymic = input("введите отчество: ")
+    date_of_birth = input("введите дату рождения: ")
     telephone = input("введите номер телефона:")
-    data = name + "; " + family + "; "+ father +"; "+ date +"; " + telephone + "; " + "\n"
-    return data
+    data = name + "; " + last_name + ";" + patronymic + "; " + date_of_birth +"; " + telephone + "; " + "\n"
+    return data.lower()
 
-# def search():
-#     str_search = input("строка для поиска  ")
-#     return str_search
-    
+def search():
+    str_search = input("строка для поиска  ").lower()
+    return str_search
+
+def select_num():
+    num = int(input("выбери строку для удаления: "))
+    return num
